39082b5bf85de74e1d3da8e65f3432c38564076f
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-10 09:47:10 -0300

    feat(#1246): building payment handlers

--- src/Modules/CloudSuite.Modules.Application/Handlers/Payments/Requests/CheckPaymentExistsByCnpjRequest.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Payments/Requests/CheckPaymentExistsByCnpjRequest.cs
@@ -2,6 +2,17 @@ namespace CloudSuite.Modules.Application.Handlers.Payments.Requests
 {
     public class CheckPaymentExistsByCnpjRequest
     {
-        
+        public class CheckExistsPaymentByCnpjRequest : IRequest<CheckExistsPaymentByCnpjRequest>
+        {
+            public Guid Id { get; private set; }
+
+            public string Cnpj { get; private set; }
+
+            public CheckExistsPaymentByCnpjRequest(string cnpj)
+            {
+                Id = Guid.NewGuid();
+                Cnpj = cnpj;
+            }
+        }
     }
 }
--- src/Modules/CloudSuite.Modules.Application/Handlers/Payments/Requests/CheckPaymentExistsByNumberRequest.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Payments/Requests/CheckPaymentExistsByNumberRequest.cs
@@ -2,6 +2,17 @@ namespace CloudSuite.Modules.Application.Handlers.Payments.Requests
 {
     public class CheckPaymentExistsByNumberRequest
     {
-        
+        public class CheckExistsPaymentByNumberRequest : IRequest<CheckExistsPaymentByNumberjRequest>
+        {
+            public Guid Id { get; private set; }
+
+            public string Number { get; private set; }
+
+            public CheckExistsPaymentByNumberRequest(string number)
+            {
+                Id = Guid.NewGuid();
+                Number = number;
+            }
+        }
     }
 }

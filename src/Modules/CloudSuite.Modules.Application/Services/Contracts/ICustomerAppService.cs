3e04fe60935357f7361ec46e1190f727f7e6044a
devops <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-11-10 11:13:31 -0500

    feat(#1250): Building Services

--- src/Modules/CloudSuite.Modules.Application/Services/Contracts/ICustomerAppService.cs
+++ src/Modules/CloudSuite.Modules.Application/Services/Contracts/ICustomerAppService.cs
@@ -1,7 +1,18 @@
+using CloudSuite.Modules.Application.ViewModels;
+
 namespace CloudSuite.Modules.Application.Services.Contracts
 {
     public interface ICustomerAppService
     {
+        Task<CustomerViewModel> GetByName(string name);
+
+        Task<CustomerViewModel> GetByEmail(string email);
+
+        Task<CustomerViewModel> GetByBusinessOwner(string BusinessOwner);
+
+        Task<CustomerViewModel> GetByCreatedOn(DateTimeOffset createdOn);
+
+        Task<CustomerViewModel> GetByCompany(Company company);
          
     }
 }

be3815a35c19ec4802a5fafcd37bf31fb8db5218
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-14 11:55:21 -0300

    "feat(#1252): building view models"

--- src/Modules/CloudSuite.Modules.Application/ViewModels/CompanyViewModel.cs
+++ src/Modules/CloudSuite.Modules.Application/ViewModels/CompanyViewModel.cs
@@ -1,7 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using CloudSuite.Modules.Commons.Valueobjects;
+
 namespace CloudSuite.Modules.Application.ViewModels
 {
     public class CompanyViewModel
     {
-        
+        [Key]
+        public Guid Id { get; set; }
+
+        [DisplayName("Cnpj da Empresa")]
+        [Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+        public Cnpj Cnpj  { get; set; }
+
+        [DisplayName("Nome Social da Empresa")]
+        [Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+        public string? SocialName { get; set; }
+
+        [DisplayName("Nome Fantasia da Empresa")]
+        [Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+        public string? FantasyName { get; set; }
+
     }
 }
--- src/Modules/CloudSuite.Modules.Application/ViewModels/PaymentViewModel.cs
+++ src/Modules/CloudSuite.Modules.Application/ViewModels/PaymentViewModel.cs
@@ -1,7 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using CloudSuite.Modules.Commons.Valueobjects;
+
 namespace CloudSuite.Modules.Application.ViewModels
 {
     public class PaymentViewModel
     {
-        
+        [Key]
+        public Guid Id { get; private set; }
+
+        [DisplayName("Número do Pagamento")]
+        public string? Number { get; private set; }
+
+        [DisplayName("Data de Pagamento")]
+        [Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+        public DateTime? PaidDate { get; private set; }
+
+        [DisplayName("Data de Vencimento do Pagamento")]
+        [Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+        public DateTime? ExpireDate { get; private set; }
+
+        [DisplayName("Total do Pagamento")]
+        [Required(ErrorMessage = "O {0} campo deve ser preenchido.")]
+        public decimal? Total { get; private set; }
+
+        [DisplayName("Total Pago")]
+        public decimal? TotalPaid { get; private set; }
+
+        [DisplayName("Pagador")]
+        public string? Payer { get; private set; }
+
+        [DisplayName("Cnpj do pagador")]
+        public Cnpj Cnpj { get; private set; }
+
+        [DisplayName("Email do Pagador")]
+        public Email Email { get; private set; }
     }
 }

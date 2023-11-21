74ffe1ec38c9605f15f982baa3c60dc9ca4d3fbf
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-21 13:39:59 -0300

    feat(#1268): building handlers from customer

--- src/Modules/CloudSuite.Modules.Application/Handlers/Customers/CreateCustomerCommand.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Customers/CreateCustomerCommand.cs
@@ -1,7 +1,43 @@
+using CloudSuite.Modules.Commons.Valueobjects;
+using CloudSuite.Modules.Domain.Models;
+using CustomerEntity = CloudSuite.Modules.Domain.Models.Customer;
+    using MediatR;
+
 namespace CloudSuite.Modules.Application.Handlers.Customers
 {
-    public class CreateCustomerCommand
+    public class CreateCustomerCommand: IRequest<CreateCustomerCommand>
     {
-        
+        public Guid Id { get; private set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Cnpj { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? BusinessOwner { get; set; }
+
+        public DateTimeOffset? CreatedOn { get; set; }
+
+        public Company Company { get; set; }
+
+        public CreateCustomerCommand()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public CustomerEntity GetEntity()
+        {
+            return new CustomerEntity(
+                new Name(this.FirstName, this.LastName),
+                new Cnpj(this.Cnpj),
+                new Email(this.Email),
+                this.BusinessOwner,
+                this.CreatedOn,
+                this.Company
+                );
+        }
     }
 }
---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Customers/CreateCustomerHandler.cs
@@ -0,0 +1,52 @@
+﻿using CloudSuite.Modules.Application.Handlers.Customers.Responses;
+using CloudSuite.Modules.Commons.Valueobjects;
+using CloudSuite.Modules.Domain.Contracts;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.Handlers.Customers
+{
+    public class CreateCustomerHandler
+    {
+        private ICustomerRepository _repositorioCustomer;
+        private readonly ILogger<CreateCustomerHandler> _logger;
+
+        public CreateCustomerHandler(ICustomerRepository repositorioCustomer, ILogger<CreateCustomerHandler> logger)
+        {
+            _repositorioCustomer = repositorioCustomer;
+            _logger = logger;
+        }
+
+        public async Task<CreateCustomerResponse> Handle(CreateCustomerCommand command, CancellationToken cancellation)
+        {
+            _logger.LogInformation($"CreateCustomerCommand: {JsonSerializer.Serialize(command)}");
+            var validationResult = new CreateCustomerCommandValidation().Validate(command);
+
+            if (validationResult.IsValid)
+            {
+                try
+                {
+                    var customer = await _repositorioCustomer.GetByCnpj(new Cnpj(command.Cnpj));
+                    if (customer != null)
+                    {
+                        return await Task.FromResult(new CreateCustomerResponse(command.Id, "cliente já foi cadastrado!"));
+                    }
+
+                    await _repositorioCustomer.Add(command.GetEntity());
+                    return await Task.FromResult(new CreateCustomerResponse(command.Id, validationResult));
+                } catch (Exception ex)
+                {
+                    _logger.LogCritical(ex.Message);
+                    return await Task.FromResult(new CreateCustomerResponse(command.Id, "Não foi possível processar a solicitação."));
+                }
+            }
+
+            return await Task.FromResult(new CreateCustomerResponse(command.Id, validationResult));
+        }
+    }
+}

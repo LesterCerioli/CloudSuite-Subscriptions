5da3f98c1b325979a093fc40d1bedc3fb1d807da
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-11-22 13:11:37 -0300

    feat9#1147): Building Services Tests

---
+++ test/CloudSuite.Modules.Application.Tests/Service3s/CompanyAppServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using CloudSuite.Modules.Application.Services.Contracts;
+using CloudSuite.Modules.Application.Services.Implementations;
+using CloudSuite.Modules.Application.ViewModels;
+using CloudSuite.Modules.Commons.Valueobjects;
+using CloudSuite.Modules.Domain.Contracts;
+using CloudSuite.Modules.Domain.Models;
+using Moq;
+using NetDevPack.Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Xunit;
+
+namespace CloudSuite.Modules.Application.Tests.Service3s
+{
+	public class CompanyAppServiceTests
+	{
+		[Fact]
+		public async Task GetByCnpj_ShouldReturnCompanyViewModel()
+		{
+			var companyRepositoryMock = new Mock<ICompanyRepository>();
+
+			var mapperMock = new Mock<IMapper>();
+
+			
+
+
+			var cnpj = new Cnpj("12345678901234"); // Replace with a valid CNPJ for testing
+			var companyModel = new CompanyViewModel(); // Replace with a sample Company model
+			var expectedViewModel = new CompanyViewModel(); // Replace with a samp
+
+			//Act
+			var result = await companyAppService.GetByCnpj(cnpj);
+
+			// Assert
+			Assert.NotNull(result);
+			Assert.Same(expectedViewModel, result);
+
+		}
+
+		[Fact]
+		public async Task GetByFantasyName_ShouldReturnCompanyViewModel()
+		{
+			var companyRepositoryMock = new Mock<ICompanyRepository>();
+			var mapperMock = new Mock<IMapper>();
+
+			
+
+			var fantasyName = "Carioca .L.C";
+			var companyModel = new CompanyViewModel();
+			
+
+		}
+	}
+}
---
+++ test/CloudSuite.Modules.Application.Tests/Service3s/CustomerAppServiceTests.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using CloudSuite.Modules.Application.Handlers.Customers;
+using CloudSuite.Modules.Application.Services.Implementations;
+using CloudSuite.Modules.Application.ViewModels;
+using CloudSuite.Modules.Domain.Contracts;
+using CloudSuite.Modules.Domain.Models;
+using Moq;
+using NetDevPack.Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CloudSuite.Modules.Application.Tests.Service3s
+{
+	public class CustomerAppServiceTests
+	{
+		
+		[Fact]
+		public async Task GetByBusinessOwner_ShouldReturnCustomerViewModel()
+		{
+			var customerRepositoryMock = new Mock<ICustomerRepository>();
+			var mapperMock = new Mock<IMapper>();
+
+			var customerAppService = new CustomerAppService(
+				customerRepositoryMock.Object,
+				mapperMock.Object,
+				It.IsAny<IMediatorHandler>());
+
+			var businessowner = "John Doe";
+
+			var customerModel = new CustomerViewModel();
+
+			var expectedViewModel = new CustomerViewModel();
+
+
+						
+		}
+
+		[Fact]
+		public void Dispose_ShouldSuppressFinalize()
+		{
+			var customerRepositoryMock = new Mock<ICustomerRepository>();
+
+			var mapperMock = new Mock<IMapper>();
+
+			var customerAppService = new CustomerAppService(
+				customerRepositoryMock.Object,
+				mapperMock.Object,
+				It.IsAny<IMediatorHandler>()
+			);
+
+			//Assert
+			customerAppService.Dispose();
+
+			// Assert
+			//customerRepositoryMock.Verify(repo => repo.Dispose(), Times.Once);
+		}
+
+		// Example of a test for Save method
+		[Fact]
+		public async Task Save_ShouldThrowNotImplementedException()
+		{
+			// Arrange
+			var customerRepositoryMock = new Mock<ICustomerRepository>();
+			var mapperMock = new Mock<IMapper>();
+
+			var customerAppService = new CustomerAppService(
+				customerRepositoryMock.Object,
+				mapperMock.Object,
+				It.IsAny<IMediatorHandler>()
+			);
+
+			var createCustomerCommand = new CreateCustomerCommand(); // Replace with a valid CreateCustomerCommand object
+
+			// Act and Assert
+			await Assert.ThrowsAsync<NotImplementedException>(() => customerAppService.Save(createCustomerCommand));
+		}
+
+
+	}
+}

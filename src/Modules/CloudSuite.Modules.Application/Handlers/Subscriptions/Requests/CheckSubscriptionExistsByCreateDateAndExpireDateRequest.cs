e8597eefac18659238b0bfff64ee46049487c96d
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-23 12:19:10 -0300

    feat(#1268): finishing handlers and validation from subscription handler

--- src/Modules/CloudSuite.Modules.Application/Handlers/Subscriptions/Requests/CheckSubscriptionExistsByCreateDateAndExpireDateRequest.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Subscriptions/Requests/CheckSubscriptionExistsByCreateDateAndExpireDateRequest.cs
@@ -6,10 +6,10 @@ namespace CloudSuite.Modules.Application.Handlers.Subscriptions.Requests
     public class CheckSubscriptionExistsByCreateDateAndExpireDateRequest : IRequest<CheckSubscriptionExistsByCreateDateAndExpireDateResponse>
     {
         public Guid Id { get; private set; }
-        public DateTime? CreateDate { get; set; }
-        public DateTime? ExpirteDate { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ExpirteDate { get; set; }
 
-        public CheckSubscriptionExistsByCreateDateAndExpireDateRequest(DateTime? createDate, DateTime? expireDate) {
+        public CheckSubscriptionExistsByCreateDateAndExpireDateRequest(DateTime createDate, DateTime expireDate) {
             Id = Guid.NewGuid();
             CreateDate = createDate;
             ExpirteDate = expireDate;
--- src/Modules/CloudSuite.Modules.Application/Handlers/Subscriptions/Requests/CheckSubscriptionExistsBySubscriptionNumberRequest.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Subscriptions/Requests/CheckSubscriptionExistsBySubscriptionNumberRequest.cs
@@ -5,7 +5,7 @@ namespace CloudSuite.Modules.Application.Handlers.Subscriptions.Requests
 {
     public class CheckSubscriptionExistsBySubscriptionNumberRequest : IRequest<CheckSubscriptionExistsBySubscriptionNumberResponse>
     {
-        public Guid ID { get; private set; }
+        public Guid Id { get; private set; }
         public string? SubscriptionNumber {  get; set; }
 
         public CheckSubscriptionExistsBySubscriptionNumberRequest(Guid id, string subscriptionNumber)

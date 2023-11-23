c9bd6e65aa77d0f94e79260d6a21b02fc88e8bd1
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-23 10:07:27 -0300

    feat(#1268): creating requests from subscription handle

--- src/Modules/CloudSuite.Modules.Application/Handlers/Subscriptions/Requests/CheckSubscriptionExistsBySubscriptionNumberRequest.cs
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Subscriptions/Requests/CheckSubscriptionExistsBySubscriptionNumberRequest.cs
@@ -1,7 +1,17 @@
+using CloudSuite.Modules.Application.Handlers.Subscriptions.Responses;
+using MediatR;
+
 namespace CloudSuite.Modules.Application.Handlers.Subscriptions.Requests
 {
-    public class CheckSubscriptionExistsBySubscriptionNumberRequest
+    public class CheckSubscriptionExistsBySubscriptionNumberRequest : IRequest<CheckSubscriptionExistsBySubscriptionNumberResponse>
     {
-        
+        public Guid ID { get; private set; }
+        public string? SubscriptionNumber {  get; set; }
+
+        public CheckSubscriptionExistsBySubscriptionNumberRequest(Guid id, string subscriptionNumber)
+        {
+            Id = Guid.NewGuid();
+            SubscriptionNumber = subscriptionNumber;
+        }
     }
 }

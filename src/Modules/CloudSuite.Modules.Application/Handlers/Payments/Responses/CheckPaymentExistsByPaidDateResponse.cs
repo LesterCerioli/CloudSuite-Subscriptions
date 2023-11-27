ca9af33e43fd46197e745194df319f19a0da44c3
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-27 17:39:50 -0300

    feat(#1268): refactoring payment handlers

---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Payments/Responses/CheckPaymentExistsByPaidDateResponse.cs
@@ -0,0 +1,27 @@
+﻿using CloudSuite.Modules.Application.Core;
+using FluentValidation.Results;
+
+namespace CloudSuite.Modules.Application.Handlers.Payments.Responses
+{
+    public class CheckPaymentExistsByPaidDateResponse : Response
+    {
+        public Guid RequestId { get; private set; }
+        public bool Exists { get; set; }
+
+        public CheckPaymentExistsByPaidDateResponse(Guid requestId, bool exists, ValidationResult result)
+        {
+            RequestId = requestId;
+            Exists = exists;
+            foreach(var item in result.Errors) {
+                this.AddError(item.ErrorMessage);
+            }
+        }
+
+        public CheckPaymentExistsByPaidDateResponse(Guid requestId, string falhaValidacao)
+        {
+            RequestId = requestId;
+            Exists = false;
+            this.AddError(falhaValidacao);
+        }
+    }
+}

4a1ad6d59fdafa4b6d901e406b7edc413e6f8614
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-11-29 06:37:39 -0300

    feat(#1147): Buiding Company and Customer Service tests

---
+++ test/CloudSuite.Modules.Application.Tests/Services/CompanyAppServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using CloudSuite.Modules.Application.Services.Implementations;
+using CloudSuite.Modules.Application.ViewModels;
+using CloudSuite.Modules.Commons.Valueobjects;
+using CloudSuite.Modules.Domain.Contracts;
+using CloudSuite.Modules.Domain.Models;
+using Moq;
+using NetDevPack.Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CloudSuite.Modules.Application.Tests.Services
+{
+	public class CompanyAppServiceTests
+	{
+		[Fact]
+		public async Task GetCompanyByCnpj_ShouldReturnsCompanyViewModel()
+		{
+			var cnpj = new Cnpj("49.859.881/0001-90");
+			var companyRepositoryMock = new Mock<ICompanyRepository>();
+			var mediatorHandlerMock = new Mock<MediatorHandler>();
+			var mapperMock = new Mock<IMapper>();
+
+			var companyAppService = new CompanyAppService(
+				companyRepositoryMock.Object,
+				mediatorHandlerMock.Object,
+				mapperMock.Object);
+
+			var companyEntity = new Company();
+			companyRepositoryMock.Setup(repo => repo.GetByCnpj(cnpj)).ReturnsAsync(companyEntity);
+
+			var expectedViewModel = new CompanyViewModel(/* create your expected view model here */);
+			mapperMock.Setup(mapper => mapper.Map<CompanyViewModel>(companyEntity)).Returns(expectedViewModel);
+
+			// Act
+			var result = await companyAppService.GetByCnpj(cnpj);
+
+			// Assert
+			Assert.Equal(expectedViewModel, result);
+
+		}
+
+		[Fact]
+		public async Task GetCompanyByCnpj_ShouldReturnsNullForNonExistentCompany()
+		{
+			// Arrange
+			var cnpj = new Cnpj("nonexistent-cnpj");
+			var companyRepositoryMock = new Mock<ICompanyRepository>();
+			var mediatorHandlerMock = new Mock<MediatorHandler>();
+			var mapperMock = new Mock<IMapper>();
+
+			var companyAppService = new CompanyAppService(
+				companyRepositoryMock.Object,
+				mediatorHandlerMock.Object,
+				mapperMock.Object);
+
+			companyRepositoryMock.Setup(repo => repo.GetByCnpj(cnpj)).ReturnsAsync((Company)null);
+
+			// Act
+			var result = await companyAppService.GetByCnpj(cnpj);
+
+			// Assert
+			Assert.Null(result);
+		}
+	}
+}
---
+++ test/CloudSuite.Modules.Application.Tests/Services/CustomerAppServiceTests.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using CloudSuite.Modules.Application.Handlers.Customers;
+using CloudSuite.Modules.Application.Services.Implementations;
+using CloudSuite.Modules.Application.ViewModels;
+using CloudSuite.Modules.Domain.Contracts;
+using CloudSuite.Modules.Domain.Models;
+using Moq;
+using NetDevPack.Mediator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CloudSuite.Modules.Application.Tests.Services
+{
+	public class CustomerAppServiceTests
+	{
+		public async Task GetCustomerByBusinessOwner_ShouldReturnCustomerViewModel()
+		{
+			// Arrange
+			var businessOwner = "John Doe";
+			var customerRepositoryMock = new Mock<ICustomerRepository>();
+			var mediatorHandlerMock = new Mock<IMediatorHandler>();
+			var mapperMock = new Mock<IMapper>();
+
+			var customerAppService = new CustomerAppService(
+				customerRepositoryMock.Object,
+				mapperMock.Object,
+				mediatorHandlerMock.Object
+			);
+
+			var customerEntity = new Customer();
+			customerRepositoryMock.Setup(repo => repo.GetByBusinessOwner(businessOwner)).ReturnsAsync(customerEntity);
+
+			var expectedViewModel = new CustomerViewModel(/* create your expected view model here */);
+			mapperMock.Setup(mapper => mapper.Map<CustomerViewModel>(customerEntity)).Returns(expectedViewModel);
+
+			// Act
+			var result = await customerAppService.GetByBusinessOwner(businessOwner);
+
+			// Assert
+			Assert.Equal(expectedViewModel, result);
+		}
+
+		[Fact]
+		public async Task Save_ShouldAddCustomerToRepository()
+		{
+			// Arrange
+			var createCustomerCommand = new CreateCustomerCommand(/* provide necessary parameters */);
+			var customerRepositoryMock = new Mock<ICustomerRepository>();
+			var mediatorHandlerMock = new Mock<IMediatorHandler>();
+			var mapperMock = new Mock<IMapper>();
+
+			var customerAppService = new CustomerAppService(
+				customerRepositoryMock.Object,
+				mapperMock.Object,
+				mediatorHandlerMock.Object
+			);
+
+			// Act
+			await customerAppService.Save(createCustomerCommand);
+
+			// Assert
+			customerRepositoryMock.Verify(repo => repo.Add(It.IsAny<Customer>()), Times.Once);
+		}
+	}
+}

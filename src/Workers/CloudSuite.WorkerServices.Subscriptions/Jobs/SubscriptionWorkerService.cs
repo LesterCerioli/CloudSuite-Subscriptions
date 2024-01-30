69421c530b79abef3c92d90c6062402690aab1e3
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-01-30 05:44:14 -0800

    "feat(#1314): Building Worker Service structure"

--- src/Workers/CloudSuite.WorkerServices.Subscriptions/Jobs/SubscriptionWorkerService.cs
+++ src/Workers/CloudSuite.WorkerServices.Subscriptions/Jobs/SubscriptionWorkerService.cs
@@ -1,11 +1,10 @@
-﻿using Microsoft.Extensions.Hosting;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CloudSuite.Modules.Application.Jobs
+namespace CloudSuite.WorkerServices.Subscriptions.Jobs
 {
 	public class SubscriptionWorkerService : BackgroundService
 	{

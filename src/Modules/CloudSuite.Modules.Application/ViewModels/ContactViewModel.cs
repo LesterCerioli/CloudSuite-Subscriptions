21195bf0d810a18ecdae360778870a74a5dca694
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-02-01 11:03:03 -0300

    "feat(#1315): building view models"

---
+++ src/Modules/CloudSuite.Modules.Application/ViewModels/ContactViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudSuite.Modules.Commons.Valueobjects;
+
+namespace CloudSuite.Modules.Application.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [DisplayName("Contact Name")]
+        [Required(ErrorMessage = "The Name field must be filled in.")]
+        public string? Name { get; set; }
+
+        [DisplayName("Contact Email")]
+        [Required(ErrorMessage = "The Social Email field must be filled in.")]
+        public string? Email { get; set; }
+
+        [DisplayName("Contact Number")]
+        [Required(ErrorMessage = "The Contact Number field must be filled in.")]
+        public string? Number { get; set; }
+
+        [DisplayName("Contact Description")]
+        public string Description { get; set; }
+    }
+}

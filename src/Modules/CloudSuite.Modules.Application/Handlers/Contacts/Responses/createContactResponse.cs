d507a2d5915fca7ab2d8d64493051543f2131423
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-02-06 13:47:06 -0800

    "feat(#1315): building Contact Domain models and Domain Contracts"

---
+++ src/Modules/CloudSuite.Modules.Application/Handlers/Contacts/Responses/createContactResponse.cs
@@ -0,0 +1,14 @@
+﻿using CloudSuite.Modules.Application.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Application.Handlers.Contacts.Responses
+{
+	public class CreateContactResponse : Response
+	{
+
+	}
+}
